87e66b126b89db14095e7a7db5c33694bf7cd3b2
Anjar Septinegara <septaranja@gmail.com>
2025-10-15 13:30:11 +0000

    vue lanjutan

---
+++ Project Vue/script/app2.js
@@ -0,0 +1,38 @@
+const app = Vue.createApp({
+  data(){
+      return {
+          state:true,
+          inputName:'',
+          names:[],
+          error:'',
+          showError:false
+      }
+  },
+  methods:{
+      addNameToList(){
+          const userName = this.inputName;
+          if(this.validate(userName)){
+              this.names.push(userName);
+              this.inputName = '';
+              this.showError = false;
+          } else {
+              this.showError = true;
+          }
+      },
+      validate(value){
+          this.error = '';
+          if(value === '' ){
+              this.error = 'Sorry, no empty name';
+              return false;
+          }
+
+          if(this.names.includes(value)){
+              this.error = 'Sorry, names must be unique';
+              return false;
+          }
+          return true
+      }
+  }
+
+
+}).mount('#app');
